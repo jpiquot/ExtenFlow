b3d0d8c6558f0f3d603063ee187d1bf4745c0954
Jérôme Piquot <jpiquot@fiveforty.fr>
2020-03-22 09:45:33 +0100

    init

--- ExtenFlow/test/Infrastructure/Messages/ExtenFlow.Messages.AbstractionsTests/CommandTest.cs
+++ ExtenFlow/test/Infrastructure/Messages/ExtenFlow.Messages.AbstractionsTests/CommandTest.cs
@@ -1,81 +1,33 @@
 using System;
 
-using FluentAssertions;
-
 using Newtonsoft.Json;
-
-using Xunit;
 
 namespace ExtenFlow.Messages.AbstractionsTests
 {
-    internal class TestCommand : Command
+    public class TestCommand : Command
     {
         [Obsolete]
         public TestCommand()
         {
         }
 
-        public TestCommand(string aggregateType, string? aggregateId, string userId) : base(aggregateType, aggregateId, userId)
-        {
-        }
-
-        public TestCommand(string aggregateType, string? aggregateId, string userId, Guid correlationId) : base(aggregateType, aggregateId, userId, correlationId)
-        {
-        }
-
         [JsonConstructor]
-        public TestCommand(string aggregateType, string? aggregateId, string userId, Guid correlationId, Guid messageId, DateTimeOffset dateTime)
+        public TestCommand(string aggregateType, string aggregateId, string userId, Guid correlationId, Guid messageId, DateTimeOffset dateTime)
             : base(aggregateType, aggregateId, userId, correlationId, messageId, dateTime)
         {
         }
     }
 
-    public class CommandBaseTest : RequestBaseTest
+    public abstract class CommandBaseTest<T> : RequestBaseTest<T> where T : ICommand
     {
-        protected T CheckCommandNewtonSonfSerialization<T>(T message) where T : ICommand
-
-        {
-            T deserializedMessage = CheckRequestNewtonSonfSerialization(message);
-            return deserializedMessage;
-        }
     }
 
-    public class CommandTest : CommandBaseTest
+    public class CommandTest : CommandBaseTest<TestCommand>
     {
-        [Theory]
-        [InlineData("testUser")]
-        [InlineData("a")]
-        [InlineData("ekjAfkd/§?klazlkzaklzalkzakl")]
-        public void CheckCommandAggregateTypeState(string aggregateType)
-        {
-            var message = new TestCommand(aggregateType, "id", "userId");
-            message.AggregateType.Should().Be(aggregateType);
-            message = new TestCommand(aggregateType, "id", "userId", Guid.NewGuid());
-            message.AggregateType.Should().Be(aggregateType);
-            message = new TestCommand(aggregateType, "id", "userId", Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now);
-            message.AggregateType.Should().Be(aggregateType);
-        }
+        protected override TestCommand Create(string aggregateType, string aggregateId, string userId, Guid messageId, Guid correlationId, DateTimeOffset dateTime)
+           => new TestCommand(aggregateType, aggregateId, userId, messageId, correlationId, dateTime);
 
-        [Fact]
-        public void CheckCommandBaseState()
-        {
-            const string userId = "toto";
-            var correlationId = Guid.NewGuid();
-            var messageId = Guid.NewGuid();
-            DateTimeOffset dateTime = DateTimeOffset.Now;
-            var message = new TestCommand("titixx", "tutuxx", userId, correlationId, messageId, dateTime);
-            message.UserId.Should().Be(userId);
-            message.CorrelationId.Should().Be(correlationId);
-            message.MessageId.Should().Be(messageId);
-            message.DateTime.Should().Be(dateTime);
-        }
-
-        [Fact]
-        public void CheckNewtonSoftJsonSerializeAndDesiarializeCommand()
-        {
-            var message = new TestCommand("AgrType", "Agr id", "Tu To Ti", Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now);
-
-            CheckCommandNewtonSonfSerialization(message);
-        }
+        protected override TestCommand Create()
+            => new TestCommand("Agtype", "aggreID", "My user", Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now);
     }
 }
--- ExtenFlow/test/Infrastructure/Messages/ExtenFlow.Messages.AbstractionsTests/QueryTest.cs
+++ ExtenFlow/test/Infrastructure/Messages/ExtenFlow.Messages.AbstractionsTests/QueryTest.cs
@@ -1,81 +1,33 @@
 using System;
 
-using FluentAssertions;
-
 using Newtonsoft.Json;
-
-using Xunit;
 
 namespace ExtenFlow.Messages.AbstractionsTests
 {
-    internal class TestQuery<T> : Query<T>
+    public class TestQuery : Query<string>
     {
         [Obsolete]
         public TestQuery()
         {
         }
 
-        public TestQuery(string aggregateType, string? aggregateId, string userId) : base(aggregateType, aggregateId, userId)
-        {
-        }
-
-        public TestQuery(string aggregateType, string? aggregateId, string userId, Guid correlationId) : base(aggregateType, aggregateId, userId, correlationId)
-        {
-        }
-
         [JsonConstructor]
-        public TestQuery(string aggregateType, string? aggregateId, string userId, Guid correlationId, Guid messageId, DateTimeOffset dateTime)
+        public TestQuery(string aggregateType, string aggregateId, string userId, Guid correlationId, Guid messageId, DateTimeOffset dateTime)
             : base(aggregateType, aggregateId, userId, correlationId, messageId, dateTime)
         {
         }
     }
 
-    public class QueryBaseTest : RequestBaseTest
+    public abstract class QueryBaseTest<TR, TQ> : RequestBaseTest<TQ> where TQ : IQuery<TR>
     {
-        protected T CheckQueryNewtonSonfSerialization<T, S>(T message) where T : IQuery<S>
-
-        {
-            T deserializedMessage = CheckRequestNewtonSonfSerialization(message);
-            return deserializedMessage;
-        }
     }
 
-    public class QueryTest : QueryBaseTest
+    public class QueryTest : QueryBaseTest<string, TestQuery>
     {
-        [Theory]
-        [InlineData("testUser")]
-        [InlineData("a")]
-        [InlineData("ekjAfkd/§?klazlkzaklzalkzakl")]
-        public void CheckQueryAggregateTypeState(string aggregateType)
-        {
-            var message = new TestQuery(aggregateType, "id", "userId");
-            message.AggregateType.Should().Be(aggregateType);
-            message = new TestQuery(aggregateType, "id", "userId", Guid.NewGuid());
-            message.AggregateType.Should().Be(aggregateType);
-            message = new TestQuery(aggregateType, "id", "userId", Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now);
-            message.AggregateType.Should().Be(aggregateType);
-        }
+        protected override TestQuery Create(string aggregateType, string aggregateId, string userId, Guid messageId, Guid correlationId, DateTimeOffset dateTime)
+           => new TestQuery(aggregateType, aggregateId, userId, messageId, correlationId, dateTime);
 
-        [Fact]
-        public void CheckQueryBaseState()
-        {
-            const string userId = "toto";
-            var correlationId = Guid.NewGuid();
-            var messageId = Guid.NewGuid();
-            DateTimeOffset dateTime = DateTimeOffset.Now;
-            var message = new TestQuery("titixx", "tutuxx", userId, correlationId, messageId, dateTime);
-            message.UserId.Should().Be(userId);
-            message.CorrelationId.Should().Be(correlationId);
-            message.MessageId.Should().Be(messageId);
-            message.DateTime.Should().Be(dateTime);
-        }
-
-        [Fact]
-        public void CheckNewtonSoftJsonSerializeAndDesiarializeQuery()
-        {
-            var message = new TestQuery("AgrType", "Agr id", "Tu To Ti", Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now);
-
-            CheckQueryNewtonSonfSerialization(message);
-        }
+        protected override TestQuery Create()
+            => new TestQuery("Agtype", "aggreID", "My user", Guid.NewGuid(), Guid.NewGuid(), DateTimeOffset.Now);
     }
 }
